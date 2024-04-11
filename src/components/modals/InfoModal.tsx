ac9a3b4a0932dc27aaafaf74ff40c99b107c7bed
MusMM03 <101513747+MusMM03@users.noreply.github.com>
2024-04-11 21:56:11 +0800

    Add files via upload

---
+++ src/components/modals/InfoModal.tsx
@@ -0,0 +1,51 @@
+import { Cell } from '../grid/Cell'
+import { BaseModal } from './BaseModal'
+
+type Props = {
+  isOpen: boolean
+  handleClose: () => void
+}
+
+export const InfoModal = ({ isOpen, handleClose }: Props) => {
+  return (
+    <BaseModal title="Cara untuk bermain" isOpen={isOpen} handleClose={handleClose}>
+     
+      <p className="text-sm text-gray-500">
+        Meneka WORDLE dalam 5 percubaan. Selepas setiap tekaan, warna jubin akan berubah untuk menunjukkan seberapa dekat tekaan anda dengan perkataan.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="س" status="correct" />
+        <Cell value="و" />
+        <Cell value="ل" />
+        <Cell value="و" />
+        <Cell value="ل" />
+      </div>
+      <p className="text-sm text-gray-500">
+        Huruf 'س' di dalam perkataan dan di posisi yang betul.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="س" />
+        <Cell value="و" />
+        <Cell value="ل" status="present" />
+        <Cell value="و" />
+        <Cell value="ل" />
+      </div>
+      <p className="text-sm text-gray-500">
+        Huruf 'ل' di dalam perkataan tapi di posisi yang salah.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="س" />
+        <Cell value="و" status="absent"/>
+        <Cell value="ل" />
+        <Cell value="و" status="absent" />
+        <Cell value="ل" />
+      </div>
+      <p className="text-sm text-gray-500">
+        Huruf 'و' tiada di dalam perkataan.
+      </p>
+    </BaseModal>
+  )
+}
